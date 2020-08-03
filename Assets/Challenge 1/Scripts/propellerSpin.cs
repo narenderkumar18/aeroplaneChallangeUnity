46ddab89a48fe8db9eb86b6aada8ee355cde2be5
Narender Kumar <narender78966@gmail.com>
2020-08-04 02:11:55 +0530

    just an updated bug fixed second project

---
+++ Assets/Challenge 1/Scripts/propellerSpin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class propellerSpin : MonoBehaviour
+{
+    public GameObject spinIt;
+    public float speedFan = 10f ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(speedFan, 0, 0);
+    }
+}
